5976fe9ef9773a4e0ab3b94df4fdc3734c006ee4
faldinaufal <faldinaufal11@gmail.com>
2022-07-04 01:54:29 +0700

    update responsive

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+const toggle = document.querySelector ('#mobile-butoon input')
+const nav = document.querySelector ('#content ul')
+
+toggle.addEventListener('click', function(){
+    nav.classList.toggle('slide')
+})
+
+console.log('Test')
